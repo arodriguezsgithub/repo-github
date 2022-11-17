6b73b3b01e3147030175b6e7627db23ebe984c24
arodriguezsgithub <a.rodriguezsen@hotmail.com>
2022-11-17 19:57:58 +0100

    refactor: new way to serve de service

--- github-api.service.ts
+++ github-api.service.ts
@@ -2,8 +2,15 @@ import { Injectable } from '@angular/core';
 import { Octokit } from "octokit";
 import { Observable, from } from 'rxjs';
 
+export interface Project {
+  name: string;
+  url: string;
+  created_at: string;
+  id: number;
+}
+
 const octokit = new Octokit({
-  auth: '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'
+  auth: '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'
 });
 
 @Injectable({
@@ -35,7 +42,33 @@ export class GithubApiService {
     }
   }
 
-  getUserProjects$(username: string): Observable<any>{
+  private async fetchProject(username: string, reponame: string): Promise<any>{
+    try {
+      const result = await octokit.request('GET /repos/{owner}/{repo}', {
+        owner: username,
+        repo: reponame
+      })
+  
+      const project: Project = {
+        name: result.data.name, 
+        url: result.data.url,
+        created_at: result.data.created_at ? result.data.created_at : '',
+        id: result.data.id
+      }
+
+      return project;
+  
+    } catch (error: any) {
+      return new Error(error);
+    }
+  }
+
+  getUserProjects$(username: string): Observable<Project[]>{
     return from(this.fetchUserProjects(username));
   }
+
+  getProject$(username: string, reponame: string): Observable<Project>{
+    return from(this.fetchProject(username, reponame));
+  }
+  
 }
