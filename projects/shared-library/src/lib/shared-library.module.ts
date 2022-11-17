d80297a4106a20b3188040be73985d33b7de490f
alejandro.rodriguezsen <alejandro.rodriguezs@dedalus.eu>
2022-11-17 15:16:38 +0100

    initial commit

---
+++ projects/shared-library/src/lib/shared-library.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { SharedLibraryService } from './shared-library.service';
+
+@NgModule({
+  declarations: [
+  ],
+  imports: [
+  ],
+  exports: [
+  ],
+  providers: [
+    SharedLibraryService
+  ]
+})
+export class SharedLibraryModule { }
